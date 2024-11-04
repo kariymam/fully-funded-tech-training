292ba8a09b2c52a42749ffdfbeadcba6b73be10c
kariymam <59487728+kariymam@users.noreply.github.com>
2024-11-04 16:50:20 -0500

    Finished #TODO: Remove brackets and clean text function on csv

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv # testing purposes
 import os
 import requests
 import pandas as pd
+
+load_dotenv() # testing purposing
 
 AIRTABLE_KEY = os.environ["airtable_key"]
 AIRTABLE_BASE_ID = os.environ["airtable_base_id"]
@@ -9,14 +12,32 @@ url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLEID}/"
 response = requests.get(url, headers={'Authorization': f'Bearer {AIRTABLE_KEY}'})
 data = response.json()
 
-# dataframe
+# dataframe with Airtable records
 df = pd.json_normalize(data['records'])
-col_order = ['fields.Name','fields.URL','fields.Instruction','fields.Eligible Residents (Cities)','fields.Description','fields.Applications','fields.Start Date','fields.Benefits', 'createdTime', 'id', 'fields.City']
-df=df.reindex(columns=col_order) # reorder columns
-df.drop(columns=['createdTime', 'id', 'fields.City'],axis=1,inplace=True)  # drop columns
 
-# save to csv
-df.to_csv('test.csv', index=False, encoding='utf-8')
+# drop 'createdTime', 'id'
+drop_cols = [0, 1]
+df.drop(df.columns[drop_cols],axis=1,inplace=True)
 
-#TODO: Figure out how to remove brackets from Eligible Residents & Benefits, and cleanup header names by removing 'fields.' from th first 8
+# strip out 'fields.' from column headers
+df.columns = df.columns.str.replace("fields.", "", regex=True)
+
+# reorder columns
+col_order = ['Name','URL','Instruction','Eligible Residents (Cities)','Description','Applications','Start Date','Benefits']
+df = df.reindex(columns=col_order)
+
+# clean text and save to csv
+csv_file_path = 'test.csv'
+df.to_csv(csv_file_path, index=False, encoding='utf-8')
+
+def clean_text(path):
+  df = pd.read_csv(path)
+  for column in df.columns:
+    df[column] = df[column].str.replace(r"[^a-zA-Z0-9\s,\/\.:\-\$]", "", regex=True)
+  return df
+
+df = clean_text(csv_file_path)
+df.to_csv(csv_file_path, index=False, encoding='utf-8')
+
+
 #TODO: Convert CSV to Markdown and append to README either here or in a frontend folder
