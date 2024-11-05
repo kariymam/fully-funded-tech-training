8ad2cdbaae0ddf910671757bfeb8ed676ce204ea
kariymam <59487728+kariymam@users.noreply.github.com>
2024-11-05 00:04:35 -0500

    Add prep_for_csv function

--- main.py
+++ main.py
@@ -13,31 +13,33 @@ url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLEID}/"
 response = requests.get(url, headers={'Authorization': f'Bearer {AIRTABLE_KEY}'})
 data = response.json()
 
-# dataframe with Airtable records
-df = pd.json_normalize(data['records'])
+# Prepare for CSV
+def prep_for_csv(data):
+   df = pd.json_normalize(data['records'])
+   # drop 'createdTime', 'id'
+   df.drop(df.columns[[0, 1]],axis=1,inplace=True)
+   # strip out 'fields.' from column headers
+   df.columns = df.columns.str.replace("fields.", "", regex=True)
+   # reorder columns
+   col_order = ['Company','Program','Instruction','Eligible Residents (Cities)','Description'] # Removed 'Applications','Start Date', 'Benefits'
+   return df.reindex(columns=col_order)
 
-# drop 'createdTime', 'id'
-df.drop(df.columns[[0, 1]],axis=1,inplace=True)
-
-# strip out 'fields.' from column headers
-df.columns = df.columns.str.replace("fields.", "", regex=True)
-
-# reorder columns
-col_order = ['Name','Program','Instruction','Eligible Residents (Cities)','Description'] # Removed 'Applications','Start Date', 'Benefits'
-df = df.reindex(columns=col_order)
-# clean text and save to csv
+# Save as CSV
+df = prep_for_csv(data)
 csv_file_path = 'test.csv'
 df.to_csv(csv_file_path, index=False, encoding='utf-8')
 
+# Clean columns in csv, excluding specific columns
 def clean_text(path, exclude_columns=[]):
-  df = pd.read_csv(path)
-  for column in df.columns:
-    if column not in exclude_columns:
+   df = pd.read_csv(path)
+   for column in df.columns:
+     if column not in exclude_columns:
         df[column] = df[column].str.replace(r"[^a-zA-Z0-9\s+,\|\/\.:\-_!?$]", "", regex=True)
-  return df
+   return df
 
 df = clean_text(csv_file_path, exclude_columns=["Description", "Program"])
 df.to_csv(csv_file_path, index=False, encoding='utf-8')
 
+# Convert CSV to Markdown table and overwrite table in README.md
 markdown_file_path = 'README.md'
-update_table(csv_file_path, markdown_file_path)
+update_table(csv_file_path, markdown_file_path) 
