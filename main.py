287376774d49837c9f6085f0dfe29c3e56c00d23
kariymam <59487728+kariymam@users.noreply.github.com>
2024-11-08 20:01:53 -0500

    Error handling for fetch_airtable

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ import requests
 import pandas as pd
 
 def fetch_airtable():
-    AIRTABLE_KEY = os.environ["airtable_key"]
-    AIRTABLE_BASE_ID = os.environ["airtable_base_id"]
-    AIRTABLE_TABLEID = os.environ["airtable_tableid"]
+    AIRTABLE_KEY = os.environ["airtable_key"] or None
+    AIRTABLE_BASE_ID = os.environ["airtable_base_id"] or None
+    AIRTABLE_TABLEID = os.environ["airtable_tableid"] or None
     url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLEID}/"
     response = requests.get(url, headers={'Authorization': f'Bearer {AIRTABLE_KEY}'})
     return response.json()
 
-
+# Prepare data for CSV
 def prepare_csv(data, path_to_export, exclude_columns: str = []):
     df = pd.json_normalize(data['records'])
 
     # Strip out 'fields.' from column headers
     df = df.rename(columns=lambda name: re.sub(r"^fields.", "", name, flags = re.M) if name.startswith('fields.') else name)
 
-    # Drop unselected columns
+    # Select specific columns
     selected_columns = ['Company','Program','Instruction','Location','Description']
     df = df.reindex(columns=selected_columns)
 
-    # Create a temp csv file to clean text columns in csv
+    # Create a temp csv file
     df.to_csv(path_to_export, index=False, encoding='utf-8')
 
     # Read CSV file
@@ -81,11 +81,7 @@ def main():
     data = fetch_airtable()
     csv_file_path = 'test.csv'
     markdown_file_path = 'README.md'
-
-    # Clean CSV multiselect text, excluding columns with markdown links
     prepare_csv(data, csv_file_path, exclude_columns=["Description", "Program"])
-
-    # Convert to Markdown table overwrite table in README.md
     update_table(csv_file_path, markdown_file_path)
 
 main()
