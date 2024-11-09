25f189426f16002676b2fdc85304d60d79a318bc
kariymam <59487728+kariymam@users.noreply.github.com>
2024-11-08 19:03:39 -0500

    Combine  csv2markdown.py separate file and main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import re
 import requests
 import pandas as pd
-from csv2markdown import update_table
 
 def fetch_airtable():
     AIRTABLE_KEY = os.environ["airtable_key"]
@@ -12,10 +11,8 @@ def fetch_airtable():
     response = requests.get(url, headers={'Authorization': f'Bearer {AIRTABLE_KEY}'})
     return response.json()
 
-# Prep for CSV
+
 def prepare_csv(data, path_to_export, exclude_columns: str = []):
-    
-    # Prep for CSV
     df = pd.json_normalize(data['records'])
 
     # Strip out 'fields.' from column headers
@@ -38,7 +35,47 @@ def prepare_csv(data, path_to_export, exclude_columns: str = []):
         temp[column] = temp[column].str.replace(r"[\[\[\]\']", "", regex=True)
     return temp.to_csv(path_to_export, index=False, encoding='utf-8')
 
+# Define Markdown table 
+def mkd_table_regex():
+    rgx = r"((\r?\n){2}|^)([^\r\n]*\|[^\r\n]*(\r?\n)?)+(?=(\r?\n){2}|$)"
+    return rgx
 
+# Find Markdown table
+def find_table(path: str) -> bool:
+    with open(path, "r") as f:
+        content = f.read()
+
+        # Use regex to find Markdown table in file content
+        try:
+            match = re.findall(mkd_table_regex(), content, flags=re.M)
+            return bool(match)
+        # Throw an error if can't determine a table exists
+        except IndexError:
+            print("Can't determine if table exists")
+            raise
+
+# Update Markdown table in README.md
+def update_table(csv_path, markdown_path):
+    df = pd.read_csv(csv_path)
+
+    # Convert CSV to Markdown
+    markdown_table = df.to_markdown()
+
+    # Run find_table check
+    check = find_table(markdown_path)
+
+    # In the markdown file
+    with open (markdown_path, 'r' ) as f:
+        content = f.read()
+        replace_table = re.sub(mkd_table_regex(), f"\n\n{markdown_table}", content, flags = re.M)
+        if check is True:
+            # Overwrite table at the end of the file
+            with open(markdown_path, 'w') as f:
+                f.write(replace_table)
+        elif check is False:
+            # Append table to the end of the file
+            with open(markdown_path, 'a') as f:
+                f.write('\n\n'+markdown_table)
 
 def main():
     data = fetch_airtable()
